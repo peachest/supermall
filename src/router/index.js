dc86a8e738f122d94dff22afbd72662611bc7c96
peachest <191250046@smail.nju.edu.cn>
2021-04-04 15:56:09 +0800

    init new project

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/about',
+    name: 'About',
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
